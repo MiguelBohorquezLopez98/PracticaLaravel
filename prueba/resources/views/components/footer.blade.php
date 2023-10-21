1bb1902e8cc03e5d33fb6229efbf8a9cb9f5e9b6
Miguel Bohórquez <dlsg1009@gmail.com>
2023-10-21 15:27:55 -0500

    Agregando footer.blade.php

---
+++ prueba/resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-white shadow dark:bg-gray-900 m-0 mt-5 bottom-0 absolute w-full">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <x-latest-posts number="2"></x-latest-posts>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
+                class="hover:underline">Blog de Miguel</a>. All Rights Reserved.</span>
+    </div>
+</footer>
