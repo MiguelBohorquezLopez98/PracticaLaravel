e7210c284d5e3ebc240a5cf60cd2574708059550
Miguel Bohórquez <dlsg1009@gmail.com>
2023-10-21 15:31:15 -0500

    Agregando web.php

--- prueba/routes/web.php
+++ prueba/routes/web.php
@@ -1,18 +1,18 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/layout', 'layouts.app');
+Route::get('/', [HomeController::class, "show"]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PostsController::class)->group(function (){
+    Route::get('/posts/create', "create");
+    Route::get('/posts/{post}', "show");
+    Route::post('/posts', "store");
+    Route::get('/posts/{post}/edit', "edit");
+    Route::patch('/posts/{post}', "update");
+    Route::delete('/posts/{post}', "destroy");
 });
+
+
