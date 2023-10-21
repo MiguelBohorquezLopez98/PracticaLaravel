c54a10872fece8c63e49f4c19bd5013197b97acb
Miguel Bohórquez <dlsg1009@gmail.com>
2023-10-21 15:26:51 -0500

    Agregando app.js

--- prueba/resources/js/app.js
+++ prueba/resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+//import 'bootstrap';
+
+import { createApp } from 'vue'
+import MyComponent from './components/MyComponent.vue'
+
+var el = document.getElementById('app');
+createApp({
+    components: {
+        MyComponent
+    }
+})
+    .mount(el)
+
